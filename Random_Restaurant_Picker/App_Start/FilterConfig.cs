d32272e29dcdf969a9a06f0bdee0048b1b343d79
alex decesare <adecesa1@my.westga.edu>
2020-12-21 19:01:52 -0500

    Added the initial files

---
+++ Random_Restaurant_Picker/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Random_Restaurant_Picker
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
